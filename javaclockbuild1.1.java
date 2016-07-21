34cd0ff1eed2af5716644a17cd2639dc6e3cedcc
Sagara <subramanya.sravan@gmail.com>
2016-07-21 12:00:26 -0400

    clockrelease1.1

---
+++ javaclockbuild1.1.java
@@ -0,0 +1,63 @@
+c class Clock{
+
+	    private int hour; // 0-23 hours
+
+	    private int minute; // 0-59 minutes
+
+	    private int second; // 0-59 seconds
+
+	    private double grain; // 0-3600 seconds
+
+	 
+
+	public Clock (int h, int m, int s) // set the clock to midnight, 0:0:0;  creating the clock
+
+	{
+
+	    hour = ( (h >= 0 && < 23) ? h : 0;
+
+	    minute = ( (m >=0 && <59) ? m : 0;
+
+	    second = ( (s >= 0 && <59) ? s : 0;
+
+	}
+
+	 
+
+	    Clock c = new Clock ();
+
+
+	public void Tick // how the clock works
+
+	 
+	    if ( second > 59)
+
+	{
+
+	    minute++;
+
+	}
+
+	    else
+
+	        if ( minute >= 3600 )
+
+	{
+
+	    hour++;
+
+	}
+
+	 
+
+	    if ( hour >=24)
+
+	{
+
+	    hour == 0;
+
+	}
+
+	 
+
+	}
